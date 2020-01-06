4a9aa902ed966f351f97f93fbf458a8555c862b8
Ilya Kupriyanov <kup-92@yandex.ru>
2020-01-06 18:06:34 +0300

    creating new Books

--- src/main/java/ru/kuper/springlearn/controller/HomeController.java
+++ src/main/java/ru/kuper/springlearn/controller/HomeController.java
@@ -3,7 +3,9 @@ package ru.kuper.springlearn.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.kuper.springlearn.model.Book;
 import ru.kuper.springlearn.repo.BookRepository;
 
 @Controller
@@ -16,10 +18,17 @@ public class HomeController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/hello")
+
+    @GetMapping
     public String getIndex(Model model) {
         model.addAttribute("books", bookRepository.findAll());
+        model.addAttribute("newBook",new Book());
         return "index";
     }
 
+    @PostMapping
+    public String createBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/";
+    }
 }
